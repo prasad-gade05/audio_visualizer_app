0eca4f9ac0651158044c3e8bb0cbc4a18136125f
Prasad Gade <prasadgade05@gmail.com>
2025-09-01 18:23:42 +0530

    deployment 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -12,12 +12,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <div className="app-layout">
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     </TooltipProvider>
   </QueryClientProvider>
